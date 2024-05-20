a4c4f4a19028f8407e48f665add365ebf91a1811
SARazzaq <abdurrazzaq962@gmail.com>
2024-05-20 19:34:12 +0530

    Update task_1a_1.py

--- task_1a_1.py
+++ task_1a_1.py
@@ -1,8 +1,8 @@
+#Please write a python program to generate all sentences where subject is in ["I", "You"] and verb is in ["Play", "Love"] and the object is in ["Cricket","Ludo"].
+
 subj=["I", "You"]
 verb=["Play", "Love"]
 obj=["Cricket", "Ludo"]
-
-
 for s in subj:
     for v in verb:
         for o in obj:
